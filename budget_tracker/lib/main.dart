5d9e58c0aaa9c3ea8a9e0048f838d31fe8354f0c
Stefan Smiljić <stefan.stefi.smiljic@gmail.com>
2026-02-03 15:30:13 +0100

    expenses working

--- budget_tracker/lib/main.dart
+++ budget_tracker/lib/main.dart
@@ -2,9 +2,12 @@ import 'package:budget_tracker/auth/bloc/auth_bloc.dart';
 import 'package:budget_tracker/auth/bloc/auth_state.dart';
 import 'package:budget_tracker/auth/repository/auth_repository_impl.dart';
 import 'package:budget_tracker/auth/ui/login_screen.dart';
+import 'package:budget_tracker/expenses/bloc/expense_event.dart';
+import 'package:budget_tracker/expenses/repository/expense_repository.dart';
 import 'package:budget_tracker/expenses/ui/expenses_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'expenses/bloc/expense_bloc.dart';
 
 void main() {
   runApp(
@@ -22,7 +25,12 @@ class MyApp extends StatelessWidget {
       home: BlocBuilder<AuthBloc, AuthState>(
         builder: (context, state) {
           if (state is Authenticated) {
-            return ExpensesScreen();
+            return BlocProvider(
+              create: (_) => ExpenseBloc(
+                ExpenseRepository(state.session.token))
+              ..add(LoadExpenses()),
+              child: ExpensesScreen(),
+            );
           }
           return LoginScreen();
         },
